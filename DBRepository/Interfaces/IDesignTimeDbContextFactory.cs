59f7ac17c3b5ec9b1356433418cef180264ebd2e
panimat <ppanimatka@gmail.com>
2019-10-06 21:36:50 +0300

    New Factories for migration

---
+++ DBRepository/Interfaces/IDesignTimeDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBRepository.Interfaces
+{
+    interface IDesignTimeDbContextFactory<out TContext> where TContext : DbContext
+    {
+        TContext CreateDbContext([NotNullAttribute] string[] args);
+    }
+
+    internal class NotNullAttribute : Attribute
+    {
+    }
+}
